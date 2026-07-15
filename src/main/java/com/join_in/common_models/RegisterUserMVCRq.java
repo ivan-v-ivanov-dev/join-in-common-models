c3be94f1f0138e2886461b9a192f041523488d2a
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-15 17:45:33 +0300

    Refactor record

--- src/main/java/com/join_in/common_models/RegisterUserMVCRq.java
+++ src/main/java/com/join_in/common_models/RegisterUserMVCRq.java
@@ -1,10 +1,9 @@
 package com.join_in.common_models;
 
-public record RegisterUserMVCRq() {
-
-    private static String firstName;
-    private static String lastName;
-    private static String email;
-    private static String password;
-
+public record RegisterUserMVCRq(
+        String firstName,
+        String lastName,
+        String email,
+        String password
+) {
 }
