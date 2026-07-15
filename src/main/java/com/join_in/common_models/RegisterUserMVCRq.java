90d8a0d44f57d45f4918f00b3c7c31c9763a2758
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-15 11:11:00 +0300

    Make RegisterUserMVCRq record

--- src/main/java/com/join_in/common_models/RegisterUserMVCRq.java
+++ src/main/java/com/join_in/common_models/RegisterUserMVCRq.java
@@ -1,32 +1,9 @@
 package com.join_in.common_models;
 
-public class RegisterUserMVCRq {
+public record RegisterUserMVCRq() {
 
-    private final String firstName;
-    private final String lastName;
-    private final String email;
-    private final String password;
-
-    public RegisterUserMVCRq(String firstName, String lastName, String email, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    private static String firstName;
+    private static String lastName;
+    private static String email;
+    private static String password;
 }
