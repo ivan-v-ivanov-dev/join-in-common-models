4440fbb93e7478a22f8fa04ed2ff19b6019ef19c
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-27 13:34:50 +0300

    Add ProfileRpRelationshipService

---
+++ src/main/java/com/join_in/common_models/ProfileRpRelationshipService.java
@@ -0,0 +1,9 @@
+package com.join_in.common_models;
+
+public record ProfileRpRelationshipService(
+        String identity,
+        String firstName,
+        String lastName,
+        String profileImage
+) {
+}
