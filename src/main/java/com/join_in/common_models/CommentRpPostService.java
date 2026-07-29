e8c331865bcdbe5e986a4d3d85531c9634028d9e
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-29 14:48:58 +0300

    Add field in CommentRpPostService

--- src/main/java/com/join_in/common_models/CommentRpPostService.java
+++ src/main/java/com/join_in/common_models/CommentRpPostService.java
@@ -1,6 +1,9 @@
 package com.join_in.common_models;
 
 public record CommentRpPostService(
+        String authorProfileImage,
+        String authorFirstName,
+        String authorLastName,
         String commentIdentity,
         String postIdentity,
         String authorIdentity,
