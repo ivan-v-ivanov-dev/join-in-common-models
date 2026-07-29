4cb93b86aac26457fc8183cc6a878fcfa63a6ad7
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-29 14:50:34 +0300

    Add field in CommentRpGatewayService

--- src/main/java/com/join_in/common_models/CommentRpGatewayService.java
+++ src/main/java/com/join_in/common_models/CommentRpGatewayService.java
@@ -1,6 +1,8 @@
 package com.join_in.common_models;
 
 public record CommentRpGatewayService(
+        String authorProfileImage,
+        String authorNames,
         String commentIdentity,
         String postIdentity,
         String authorIdentity,
