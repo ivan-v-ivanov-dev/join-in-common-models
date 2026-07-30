08b6ee505bcf1725c90b2b792f23f310104404ca
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-07-30 17:06:01 +0300

    Add CommentReactionsCountRpReactionService and PostReactionsCountRpReactionService

---
+++ src/main/java/com/join_in/common_models/CommentReactionsCountRpReactionService.java
@@ -0,0 +1,7 @@
+package com.join_in.common_models;
+
+public record CommentReactionsCountRpReactionService(
+        int likeCount,
+        int dislikeCount
+) {
+}
---
+++ src/main/java/com/join_in/common_models/PostReactionsCountRpReactionService.java
@@ -0,0 +1,9 @@
+package com.join_in.common_models;
+
+public record PostReactionsCountRpReactionService(
+        int likeCount,
+        int dislikeCount,
+        int hahaCount,
+        int angryCount
+) {
+}
