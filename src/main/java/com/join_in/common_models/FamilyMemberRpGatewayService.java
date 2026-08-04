3a32ba1bd5da548bd3f1fb14a9ded1796fe6810b
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-08-04 16:10:19 +0300

    Add FamilyMemberRpGatewayService and FamilyMemberRpRelationshipService

---
+++ src/main/java/com/join_in/common_models/FamilyMemberRpGatewayService.java
@@ -0,0 +1,10 @@
+package com.join_in.common_models;
+
+public record FamilyMemberRpGatewayService(
+        String identity,
+        String relationshipType,
+        String profileImage,
+        String firstName,
+        String lastName
+) {
+}
---
+++ src/main/java/com/join_in/common_models/FamilyMemberRpRelationshipService.java
@@ -0,0 +1,10 @@
+package com.join_in.common_models;
+
+public record FamilyMemberRpRelationshipService(
+        String identity,
+        String relationshipType,
+        String profileImage,
+        String firstName,
+        String lastName
+) {
+}
