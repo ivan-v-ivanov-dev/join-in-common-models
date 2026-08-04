9c4b9da62a301d52cfaa45450bbc29b1cfe83c28
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-08-04 11:37:37 +0300

    Add NotificationRpNotificationService

---
+++ src/main/java/com/join_in/common_models/NotificationRpNotificationService.java
@@ -0,0 +1,12 @@
+package com.join_in.common_models;
+
+public record NotificationRpNotificationService(
+        String authorIdentity,
+        String authorProfileImage,
+        String authorFirstName,
+        String authorLastName,
+        String postIdentity,
+        String content,
+        String postedAgo
+) {
+}
