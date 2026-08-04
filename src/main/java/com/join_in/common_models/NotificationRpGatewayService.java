33ab1be9e314f6c90f6b6a07ee0407bf7c43a88e
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-08-04 12:24:05 +0300

    Add NotificationRpGatewayService

---
+++ src/main/java/com/join_in/common_models/NotificationRpGatewayService.java
@@ -0,0 +1,12 @@
+package com.join_in.common_models;
+
+public record NotificationRpGatewayService(
+        String authorIdentity,
+        String authorProfileImage,
+        String authorFirstName,
+        String authorLastName,
+        String postIdentity,
+        String content,
+        String postedAgo
+) {
+}
