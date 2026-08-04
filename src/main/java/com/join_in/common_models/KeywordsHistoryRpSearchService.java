6fbdb70cf532ebaf879a454b3233cb765e9afbf0
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-08-04 10:08:33 +0300

    Delete KeywordsHistoryRpSearchService and SearchHistoryRpSearchService

--- src/main/java/com/join_in/common_models/KeywordsHistoryRpSearchService.java
+++
@@ -1,9 +0,0 @@
-package com.join_in.common_models;
-
-import java.time.LocalDateTime;
-
-public record KeywordsHistoryRpSearchService(
-        String keyword,
-        LocalDateTime searchedAt
-) {
-}
