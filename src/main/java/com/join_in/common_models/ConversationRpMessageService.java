cc40c078a221d6b4107bac66c1cb2d6d7e69b6ce
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-08-10 12:16:48 +0300

    Add ConversationRpMessageService

--- src/main/java/com/join_in/common_models/ConversationRpMessageService.java
+++ src/main/java/com/join_in/common_models/ConversationRpMessageService.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 public record ConversationRpMessageService(
     String profileIdentity,
+    String profileNames,
+    String profileImage,
     LocalDateTime lastMessageAt,
     String conversationIdentity,
     String participantIdentity
