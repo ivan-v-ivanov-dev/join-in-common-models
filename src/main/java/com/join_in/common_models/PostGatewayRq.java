c9802d8241c0f88d88ab88cd2f209ba007f63dc8
Ivan Ivanov <ivan.v.ivanov.dev@gmail.com>
2026-09-02 10:41:35 +0300

    Add  String groupIdentity

--- src/main/java/com/join_in/common_models/PostGatewayRq.java
+++ src/main/java/com/join_in/common_models/PostGatewayRq.java
@@ -3,6 +3,7 @@ package com.join_in.common_models;
 import java.util.List;
 
 public record PostGatewayRq(
+        String groupIdentity,
         String content,
         byte[] imageBytes,
         String youtubeUrl,
